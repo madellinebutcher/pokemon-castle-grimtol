e9a3491cad34822169fa6b10917e1f56c5b44002
Madelline Butcher <madellinebutcher@gmail.com>
2018-06-21 11:35:05 -0600

    changes

---
+++ Project/Interfaces/IRoom.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace CastleGrimtol.Project
+{
+    public interface IRoom
+    {
+        string Name { get; set; }
+        string Description { get; set; }
+        List<Item> Items { get; set; }
+
+        void UseItem(Item item);
+
+    }
+}
