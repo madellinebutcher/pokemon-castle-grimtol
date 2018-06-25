f9b263ba5c63c6484ad5f01b23dae29098c3aa5f
Madelline Butcher <madellinebutcher@gmail.com>
2018-06-24 23:37:27 -0600

    decided to just have a one item pick up for now

--- Project/Game.cs
+++ Project/Game.cs
@@ -15,14 +15,14 @@ namespace CastleGrimtol.Project
         public void Setup()
         {
             Room CaveEntrance = new Room("Cave Entrance", "They created a smokescreen and disappears. You think they ran further into the cave and chase after them, but come across two entrances. Which do you take? [Left or Right]");
-            Room CaveLeft = new Room("Cave Left", "You decide to go left and you trip over something. You take a look at it and it seems to be a little lump on the ground. You inspect it. It seems to be really hard and buried into the ground. You try to dig it out, but all of a sudden it pops up and a pokemon is right under it. It is a Diglett and on its head is a Master Pokeball. Diglett freaks out and starts running away with the Master Pokeball. Bulbasuar tries to block it before it gets away. You look in your backpack and you have [a berry, a Pokeball] What do you decide to do? [Forward or Back].");
-            Room CaveOnyx = new Room("Cave Onyx", "Diglett happily eats the berry. You [Take Masterball] from the top of his head. Squirtle is teasing Charmander and makes him angry. Charmander uses flamethrower at Squirtle who dodges before being hit and Charmander's flamerthrower hit this huge rock instead. All of a sudden the rock moves. It turns around and you are staring right into the face of Onyx. He does not look too happy. All your pokemon are scared.[In your backpack you have a Pokeball and a Masterball or Forward(run) or Back(go)]. [Forward, Back]");
+            Room CaveLeft = new Room("Cave Left", "You decide to go left and you trip over something. You take a look at it and it seems to be a little lump on the ground. You inspect it. It seems to be really hard and buried into the ground. You try to dig it out, but all of a sudden it pops up and a pokemon is right under it. It is a Diglett and on its head is a Master Pokeball. Diglett freaks out and starts running away with the Master Pokeball. Bulbasuar tries to block it before it gets away. You happen to have a berry and give it to Diglett. Do you want to follow it? [Forward or Back].");
+            Room CaveOnyx = new Room("Cave Onyx", "Diglett happily eats the berry. You [Take Masterball] from the top of his head. Squirtle is teasing Charmander and makes him angry. Charmander uses flamethrower at Squirtle who dodges before being hit and Charmander's flamerthrower hit this huge rock instead. All of a sudden the rock moves. It turns around and you are staring right into the face of Onyx. He does not look too happy. All your pokemon are scared.[In your backpack you have Masterball or Forward(run) or Back(go)]. [Forward, Back]");
             Room CaveVileplum = new Room("Cave Vileplum", "Squirtle gets some spunk in him and he uses watergun on Onyx but that just makes him more angry. Onyx charges you and your pokemon. You all run away from Onyx and jump out of the way as Onyx boulders his way through the cave wall. You look into the new passage way and see all these mushrooms. Do you go [Forward or Back]");
-            Room CaveJigglypuff = new Room("Cave Jigglypuff", "You walk through all the mushrooms slowly and notice that they are Vileplums. Bulbasaur touches one and a puff of sleeping powder was sprayed onto his face. You pick him up and notice a Jigglypuff standing on a rock. It smiles at you and starts to sing its song. You get groggy and everything turns black. When you come to all your pokemon are asleep around you and has marker drawings on their face. You seem to have some on your face too! Seems like Jigglypuff did not like you sleeping during her performance. You notice that there is a little Chesto Berry tree blooming next to the spring. [get Chesto Berry] You feed your pokemon the Chesto Berry and they start to wake up. You notice that there is another entrance to the spring, but there are two entrances [Left or Right]");
-            Room TeamRocket = new Room("Team Rocket", "You took the left entrance and you see Team Rocket! You told Charmande to do flamethrower, Squirtle to do watergun and Bulbasaur to do razor leaf. The attacks hit Team Rocket and it dazes them for a bit. You run over and release Pikachu from its cage. You tell Pikachu to do Thunderbolt at Team Rocket. It was a direct hit and they go flying off! 'Looks like we are blasting off again'. Suddenly a little pokemon floats right on top of you and giggles. 'Mew' it says! What did you want to do? [Pokeball, Master Pokeball]");
-            Room Nothing = new Room("Nothing", "You took the right entrance but there is no one there. You see lots of eyes appear above you! It seems like a whole collection of Noibats! What do you want to do? [Fight, Flee, or in your backpack a Pokeball, a Masterball]. There doesn't seem to be an exit. [Go Back].");
+            Room CaveJigglypuff = new Room("Cave Jigglypuff", "You walk through all the mushrooms slowly and notice that they are Vileplums. Bulbasaur touches one and a puff of sleeping powder was sprayed onto his face. You pick him up and notice a Jigglypuff standing on a rock. It smiles at you and starts to sing its song. You get groggy and everything turns black. When you come to all your pokemon are asleep around you and has marker drawings on their face. You seem to have some on your face too! Seems like Jigglypuff did not like you sleeping during her performance. You notice that there is a little Chesto Berry tree blooming next to the spring. You feed your pokemon the Chesto Berry and they start to wake up. You notice that there is another entrance to the spring, but there are two entrances [Left or Right]");
+            Room TeamRocket = new Room("Team Rocket", "You took the left entrance and you see Team Rocket! You told Charmande to do flamethrower, Squirtle to do watergun and Bulbasaur to do razor leaf. The attacks hit Team Rocket and it dazes them for a bit. You run over and release Pikachu from its cage. You tell Pikachu to do Thunderbolt at Team Rocket. It was a direct hit and they go flying off! 'Looks like we are blasting off again'. Suddenly a little pokemon floats right on top of you and giggles. 'Mew' it says! What did you want to do? [Master Pokeball]");
+            Room Nothing = new Room("Nothing", "You took the right entrance but there is no one there. You see lots of eyes appear above you! It seems like a whole collection of Noibats! Your pokemon can't see all the Noibats. What do you want to do? There doesn't seem to be an exit. [Go Back].");
 
-            Room CaveRight = new Room("Cave Right", "You decide to go right. It is very dark. You ask Charmander to lead the way with his tail. The pokemon start to smell something. They lead you to a entrance to the left and there seems to be a stack of yummy berries behind a boulder. You let them eat some. Do you want to take one for the road? [Pick up Item or Discard]. Go [Left or Back].");
+            Room CaveRight = new Room("Cave Right", "You decide to go right. It is very dark. You ask Charmander to lead the way with his tail. The pokemon start to smell something. They lead you to a entrance to the left and there seems to be a stack of yummy berries behind a boulder. You let them eat some. Go [Left or Back].");
 
             //CaveEntrance
             CaveEntrance.Exits.Add("right", CaveRight);
@@ -54,13 +54,15 @@ namespace CastleGrimtol.Project
             //----------This section is for Items and what Room they are found in----------\\
 
             //CaveLeft
-            Item Masterball = new Item("Masterball", "Better save it for a rare and strong Pokemon");
+            Item Masterball = new Item("Masterball", "Can catch any pokemon. Save it for a rare one.");
+
 
 
             //Adding Items to rooms
 
             //CaveLeft
             CaveOnyx.Items.Add(Masterball);
+
 
 
 
@@ -129,9 +131,7 @@ namespace CastleGrimtol.Project
                     break;
                 case "take":
                 case "t":
-                    {
-                        TakeItem(input2);
-                    }
+                    TakeItem(input2);
                     break;
                 case "use":
                 case "u":
